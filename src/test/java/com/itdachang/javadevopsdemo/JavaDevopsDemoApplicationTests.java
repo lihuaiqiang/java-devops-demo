cec5a9b09d6bec2eabd1c8226e1ad653df35f996
lihq <lihq@primeton.com>
2021-04-20 17:44:09 +0800

    代码初始化提交

---
+++ src/test/java/com/itdachang/javadevopsdemo/JavaDevopsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itdachang.javadevopsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDevopsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
